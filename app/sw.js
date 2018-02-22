82acc96a6c781f523f26b8bb0b2cea32b1c80ddd
Refik Kerimi <r.kerimi@xne.at>
2018-02-22 10:19:35 +0100

    some changes for sw to work correct

--- app/sw.js
+++ app/sw.js
@@ -11,6 +11,6 @@ self.addEventListener('activate', event => {
     return self.clients.claim();
 }),
 
-self.addEventListener('fetch', event => {
+/*self.addEventListener('fetch', event => {
     event.respondWith(fetch(event.request));
-})
+})*/
